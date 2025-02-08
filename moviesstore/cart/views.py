0010058b96ac72ed17d16a6ed0c469c65b664c6f
train013 <176448038+train013@users.noreply.github.com>
2025-02-07 21:25:35 -0600

    added functionality to remove movies from cart and finished chapter 10

--- moviesstore/cart/views.py
+++ moviesstore/cart/views.py
@@ -25,3 +25,7 @@ def add(request, id):
     cart[id] = request.POST['quantity']
     request.session['cart'] = cart
     return redirect('cart.index')
+
+def clear(request):
+    request.session['cart'] = {}
+    return redirect('cart.index')
